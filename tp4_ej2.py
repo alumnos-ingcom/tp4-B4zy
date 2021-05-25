aec2d576fc0072a211f810ef54866e7f66e4c8c9
Tomás Gadea <39812448+B4zy@users.noreply.github.com>
2021-05-25 03:14:58 -0300

    Add files via upload

---
+++ tp4_ej2.py
@@ -0,0 +1,24 @@
+################
+# Tomás Gadea - @B4zy
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def suma_lenta(numero, otro_numero):
+    resultado = numero + otro_numero
+    if resultado < 0 or (numero > 0 and otro_numero < 0):
+        while numero >= resultado:
+            numero = numero - 1
+            mostrar = numero + 1
+            print(mostrar)
+    elif resultado > 0 or (numero < 0 and otro_numero > 0):
+        while numero <= resultado:
+            numero = numero + 1
+            mostrar = numero - 1
+            print(mostrar)
+    
+def prueba():
+    primer_numero = int(input("Ingrese un entero: "))
+    segundo_numero = int(input("Ingrese otro entero: "))
+    suma_lenta(primer_numero, segundo_numero)
+prueba()
