e3555258c4e6daf8c54210f72a8cbd394e21c523
Tomás Gadea <39812448+B4zy@users.noreply.github.com>
2021-05-27 22:03:54 -0300

    Add files via upload

---
+++ tp4_ej11.py
@@ -0,0 +1,24 @@
+################
+# Tomás Gadea - @B4zy
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+def es_palindromo(texto):
+    palindromo = list(texto)
+    palindromo_reverse = list(reversed(palindromo))
+    if palindromo == palindromo_reverse:
+        return True
+    else:
+        return False
+
+def prueba():
+    
+    mostrar = es_palindromo(input("Ingresa algo: "))
+    if mostrar == True:
+        print(f"[{mostrar}] Es palíndromo")
+    else:
+        print(f"[{mostrar}] No es palíndromo")
+        
+prueba()
