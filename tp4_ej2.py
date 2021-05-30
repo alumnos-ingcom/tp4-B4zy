2d28d17d2cdef24cfecea5d63a47f0d39b3e6f99
Tomás Gadea <39812448+B4zy@users.noreply.github.com>
2021-05-29 22:21:59 -0300

    Update tp4_ej2.py

--- tp4_ej2.py
+++ tp4_ej2.py
@@ -6,10 +6,14 @@
 
 def suma_lenta(numero, otro_numero):
     resultado = numero + otro_numero
-    if resultado < 0 or (numero > 0 and otro_numero < 0):
-        while numero > resultado:
-            numero = numero - 1
-    elif resultado > 0 or (numero < 0 and otro_numero > 0):
+    if (resultado < 0) or (numero > 0 and otro_numero < 0):
+        if (numero < 0 and otro_numero > 0):
+            while numero != resultado:
+                numero = numero + 1
+        else:
+            while numero > resultado:
+                numero = numero - 1
+    elif (resultado > 0) or (numero < 0 and otro_numero > 0):
         while numero < resultado:
             numero = numero + 1
             
@@ -19,6 +23,6 @@ def prueba():
     primer_numero = (int(input("Ingrese un numero: ")))
     segundo_numero = (int(input("Ingrese otro numero: ")))
     resultado = suma_lenta(primer_numero, segundo_numero)
-    print (resultado)
+    print (f"El resultado es {resultado}")
 
 prueba()
